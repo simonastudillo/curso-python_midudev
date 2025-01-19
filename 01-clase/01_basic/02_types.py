98db0fcf7fb76dd83f2fc5fd4cb90a136da56ae9
Simon Astudillo <simon.astudillo@raef.cl>
2025-01-18 22:17:13 -0300

    clase 1 - tipos de variables

---
+++ 01-clase/01_basic/02_types.py
@@ -0,0 +1,35 @@
+###
+# 02 - Tipos de datos
+# Python tiene varios tipos de datos
+# int, float, complex, str, bool, NoneType, list, tuple, dict, range, set...
+###
+
+print("int")
+print(type(10))
+print(type(0))
+print(type(-5))
+print(type(10))
+print(type(1365645632186765646546532))
+
+print("float")
+print(type(10.0))
+print(type(3.14))
+print(type(1e3))
+
+print("complex")
+print(type(1 + 2j))
+
+print("str")
+print(type("hola"))
+print(type(""))
+print(type("123"))
+print(type("""hola
+           mundo multinea"""))
+
+print("bool")
+print(type(True))
+print(type(False))
+print(type(1 < 2))
+
+print("NoneType")
+print(type(None))
--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 ## 1. Hola mundo
 - 01-clase/01_basic/01_print.py
+
+## 2. Tipos de datos
+- 02-clase/02_basic/02_types.py
