30af2d7daf84abc1d9da7d91264b5cc0190766be
Simon Astudillo <simon.astudillo@raef.cl>
2025-01-18 22:17:57 -0300

    clase 1 - comentarios

---
+++ 01-clase/01_basic/00_comments.py
@@ -0,0 +1,7 @@
+### esto es un comentario
+# esto tambien es un comentario
+"""
+esto es un comentario multilinea
+en realidad es un string que al no ser asignado a una variable
+lo toma como un comentario
+"""
--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Temario
 
+## 0. Comentarios
+- 01-clase/01_basic/02_comments.py
+
 ## 1. Hola mundo
 - 01-clase/01_basic/01_print.py
 
