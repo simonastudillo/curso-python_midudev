ba44d5c2a5b3860b9bf2b129100c205fbca36cad
Simon Astudillo <simon.astudillo@raef.cl>
2025-01-30 23:37:59 -0300

    clase 3 - 02.resolucion ejercicios

--- 03_loops/02_exercises.py
+++ 03_loops/02_exercises.py
@@ -6,17 +6,36 @@
 # Imprime todos los números pares del 2 al 20 (inclusive) usando un bucle for.
 print("\nEjercicio 1:")
 
+for numero in range(2, 21):
+  if numero % 2 == 0:
+    print(numero)
+
 # Ejercicio 2: Calcular la media de una lista
 # Dada la siguiente lista de números:
 # numeros = [10, 20, 30, 40, 50]
 # Calcula la media de los números usando un bucle for.
 print("\nEjercicio 2:")
 
+numeros = [10, 20, 30, 40, 50]
+media = 0
+for numero in numeros:
+  media += numero
+media /= len(numeros)
+print(f"La media es {media}")
+
+
 # Ejercicio 3: Buscar el máximo de una lista
 # Dada la siguiente lista de números:
 # numeros = [15, 5, 25, 10, 20]
 # Encuentra el número máximo en la lista usando un bucle for.
 print("\nEjercicio 3:")
+
+numeros = [15, 5, 25, 10, 20]
+numero_maximo = 0
+for numero in numeros:
+  if numero > numero_maximo:
+    numero_maximo = numero
+print(f"El máximo es {numero_maximo}")
 
 # Ejercicio 4: Filtrar cadenas por longitud
 # Dada la siguiente lista de palabras:
@@ -25,9 +44,24 @@ print("\nEjercicio 3:")
 # usando un bucle for y list comprehension.
 print("\nEjercicio 4:")
 
+palabras = ["casa", "arbol", "sol", "elefante", "luna"]
+palabras_filtradas = []
+for palabra in palabras:
+  if len(palabra) > 5:
+    palabras_filtradas.append(palabra)
+print(palabras_filtradas)
+
 # Ejercicio 5: Contar palabras que empiezan con una letra
 # Dada la siguiente lista de palabras:
 # palabras = ["casa", "arbol", "sol", "elefante", "luna", "coche"]
 # Pide al usuario que introduzca una letra.
 # Cuenta cuántas palabras en la lista empiezan con esa letra (sin diferenciar mayúsculas/minúsculas).
 print("\nEjercicio 5:")
+
+palabras = ["casa", "arbol", "sol", "elefante", "luna", "coche"]
+palabras_filtradas = []
+letra = input("Introduzca una letra: ")
+for palabra in palabras:
+  if palabra[0] == letra.lower():
+    palabras_filtradas.append(palabra)
+print(f"Se han encontrado {len(palabras_filtradas)} palabras que empiezan con {letra}")
