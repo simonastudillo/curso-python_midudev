a7f09537ceb4821d06353ce15b44a6f79c372093
Simon Astudillo <simon.astudillo@raef.cl>
2025-02-07 22:27:58 -0300

    clase 04 - 04.diccionarios

---
+++ 04_logic/04_dictionaries.py
@@ -0,0 +1,18 @@
+###
+# 04 - Dictionaries
+# Los diccionarios son colecciones de pares clave-valor.
+# Sirven para almacenar datos relacionados.
+###
+
+# ejemplo tipico de diccionario
+persona = {
+  "nombre": "midudev",
+  "edad": 25,
+  "es_estudiante": True,
+  "calificaciones": [7, 8, 9],
+  "socials": {
+    "twitter": "@midudev",
+    "instagram": "@midudev",
+    "facebook": "midudev"
+  }
+}
