ca3352b8701ac6b82b9d4cc40f570d8064da06ea
Simon Astudillo <simon.astudillo@raef.cl>
2025-02-07 22:27:04 -0300

    clase 04 - 03.challenge solved

--- 04_logic/03_challenge_find_first_sum.py
+++ 04_logic/03_challenge_find_first_sum.py
@@ -6,3 +6,26 @@ goal = 8
 
 find_first_sum(nums, goal)  # [2, 3]
 """
+#Mi función
+def find_first_sum(nums, goal):
+  indices = None
+  for index, i in enumerate(nums):
+    for j in range(index+1, len(nums)):
+      if i + nums[j] == goal:
+        return [index, j]
+  return indices
+
+# Función midu
+def find_first(nums, goal):
+  for i in range(len(nums)):
+    for j in range( i+1, len(nums)):
+      if nums[i] + nums[j] == goal:
+        return [i, j]
+  return None
+
+
+nums = [4, 5, 6, 3, 5, 4]
+goal = 8
+
+print(find_first_sum(nums, goal))
+print(find_first(nums, goal))
